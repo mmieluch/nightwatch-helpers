c1b0b8ac357ead7dfcd1d2804e513f9ac23f574b
Michal Mieluch <michal.mieluch@bluefroglondon.com>
2017-05-17 14:39:06 +0100

    Add "scrollTo" command

--- dist/commands/scrollTo.js
+++ dist/commands/scrollTo.js
@@ -2,6 +2,20 @@
 
 var events = require('events');
 
+/**
+ * Get the name of the selector. Helpful when using page objects.
+ * Currently Nightwatch does not know how to handle identifiers in some commands,
+ * if they are page object elements' identifiers (starting with '@' sign).
+ *
+ * This method returns the selector after "@" or the entire identifier string.
+ *
+ * @param {String} identifier
+ * @returns {String}
+ */
+var getSelector = function getSelector(identifier) {
+  return identifier[0] === '@' ? this.elements[identifier.substring(1)].selector : identifier;
+};
+
 var classCallCheck = function (instance, Constructor) {
   if (!(instance instanceof Constructor)) {
     throw new TypeError("Cannot call a class as a function");
@@ -86,16 +100,85 @@ var scrollTo = function (_EventEmitter) {
     key: 'command',
 
     /**
+     * @param {string} [selector = null]
      * @returns {scrollTo}
      */
     value: function command() {
-      console.log('scrolling to the bastard!');
-      return this.complete();
+      var _this2 = this;
+
+      var selector = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+
+      // No selector === no work. Like me, but with coffee.
+      if (selector === null) return this.complete();
+      // Prepare to tell the world the job is done.
+      var emitComplete = this.emit.bind(this, 'complete');
+      // Transform the selector, in case the "@" sign is too baffling for Nightwatch.
+      var sel = getSelector(selector);
+
+      // Establish the location of the element.
+      this.getLocation(sel)
+      // Set window position to the found coordinates.
+      .then(function (coordinates) {
+        return _this2.scrollTo(coordinates.y);
+      }).then(emitComplete).catch(function (err) {
+        console.error(err);
+        emitComplete();
+      });
+
+      return this;
+    }
+
+    /**
+     * Promisify `setWindowPosition` to avoid callback hell.
+     * @param {number} [y = 0]
+     * @returns {Promise}
+     */
+
+  }, {
+    key: 'scrollTo',
+    value: function scrollTo() {
+      var y = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+
+      if (y === 0) console.warn('The value for the `y` coordinate was passed in as 0.');
+
+      var self = this;
+
+      return new Promise(function (resolve, reject) {
+        self.api.execute(function (y) {
+          window.scroll(0, y);
+        }, [y], function (result) {
+          resolve(true);
+        });
+      });
+    }
+
+    /**
+     * Promisify `getLocation` function to avoid callback hell.
+     * @param selector
+     * @returns {Promise}
+     */
+
+  }, {
+    key: 'getLocation',
+    value: function getLocation(selector) {
+      var self = this;
+
+      return new Promise(function (resolve, reject) {
+        self.api.getLocation(selector, function (result) {
+          if (result.status !== 0) reject(result);
+
+          resolve({
+            x: result.value.x,
+            y: result.value.y
+          });
+        });
+      });
     }
 
     /**
      * Emit a "complete" event and return the command instance.
      * @fires EventEmitter#complete
+     * @returns {scrollTo}
      */
 
   }, {
--- src/commands/scrollTo.js
+++ src/commands/scrollTo.js
@@ -1,4 +1,5 @@
 import { EventEmitter } from 'events'
+import getSelector from '../utils/get-selector'
 
 /**
  * @class scrollTo
@@ -7,16 +8,73 @@ import { EventEmitter } from 'events'
  */
 export default class scrollTo extends EventEmitter {
   /**
+   * @param {string} [selector = null]
    * @returns {scrollTo}
    */
-  command () {
-    console.log('scrolling to the bastard!')
-    return this.complete()
+  command (selector = null) {
+    // No selector === no work. Like me, but with coffee.
+    if (selector === null) return this.complete()
+    // Prepare to tell the world the job is done.
+    const emitComplete = this.emit.bind(this, 'complete')
+    // Transform the selector, in case the "@" sign is too baffling for Nightwatch.
+    const sel = getSelector(selector)
+
+    // Establish the location of the element.
+    this.getLocation(sel)
+      // Set window position to the found coordinates.
+      .then(coordinates => this.scrollTo(coordinates.y))
+      .then(emitComplete)
+      .catch(err => {
+        console.error(err)
+        emitComplete()
+      })
+
+    return this
+  }
+
+  /**
+   * Promisify `setWindowPosition` to avoid callback hell.
+   * @param {number} [y = 0]
+   * @returns {Promise}
+   */
+  scrollTo (y = 0) {
+    if (y === 0) console.warn('The value for the `y` coordinate was passed in as 0.')
+
+    const self = this
+
+    return new Promise(function (resolve, reject) {
+      self.api.execute(function (y) {
+        window.scroll(0, y)
+      }, [y], function (result) {
+        resolve(true)
+      })
+    })
+  }
+
+  /**
+   * Promisify `getLocation` function to avoid callback hell.
+   * @param selector
+   * @returns {Promise}
+   */
+  getLocation (selector) {
+    const self = this
+
+    return new Promise(function (resolve, reject) {
+      self.api.getLocation(selector, function (result) {
+        if (result.status !== 0) reject(result)
+
+        resolve({
+          x: result.value.x,
+          y: result.value.y,
+        })
+      })
+    })
   }
 
   /**
    * Emit a "complete" event and return the command instance.
    * @fires EventEmitter#complete
+   * @returns {scrollTo}
    */
   complete () {
     this.emit('complete')
